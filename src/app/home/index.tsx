fde4d95686670df851af5ae03dbf332593ee9636
MiguelProgramas <miguel.p.rograma.lima.tsis@gmail.com>
2024-09-06 13:51:33 -0300

    :sparkles: feat: Confecção do avatar.

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -7,17 +7,24 @@ import { Input } from '../components/input'
 
 import { theme } from '@/theme'
 
+import { useState } from 'react';
+
+import { Contact } from '@/app/components/contact'
+
 
 export function Home() {
+    const [name, setName] = useState("")
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Input style={styles.input}>
                 <Feather name="search" size={16} color={theme.colors.gray_300}></Feather>
-                    <Input.Field placeholder='Pesquisar pelo nome...'/>
-                <Feather name="x" size={16} color={theme.colors.gray_300}></Feather>
+                    <Input.Field placeholder='Pesquisar pelo nome...' onChangeText={setName} value={name}/>
+                <Feather name="x" size={16} color={theme.colors.gray_300} onPress={() => setName("")}></Feather>
                 </Input>
             </View>
+            <Contact />
         </View>
     )
 }
