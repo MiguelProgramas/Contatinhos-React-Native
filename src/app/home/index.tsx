9f1aebc7cf224c804032a01ff02fb4bb66779226
MiguelProgramas <miguel.p.rograma.lima.tsis@gmail.com>
2024-09-12 09:40:44 -0300

    :sparkles: feat: Sincronização dos contatos.

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,19 +1,34 @@
-import { View } from 'react-native';
-import { Feather } from '@expo/vector-icons'
+import { Alert, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { styles } from './styles';
+import { Input } from '../components/input';
+import { theme } from '@/theme';
+import { useState, useEffect } from 'react';
+import { Contact } from '@/app/components/contact';
+import * as Contacts from 'expo-contacts';
 
-import { styles } from './styles'
+async function fetchContacts() {
+    try {
+        const { status } = await Contacts.requestPermissionsAsync();
+        if (status === Contacts.PermissionStatus.GRANTED){
+            const { data } = await Contacts.getContactsAsync();
+            console.log(data);
+        }
+    }
+    catch(error) {
+        console.log(error);
+        Alert.alert("Contatos", "Não foi possível carregar os contatos...");
+    }
+}
 
-import { Input } from '../components/input'
-
-import { theme } from '@/theme'
-
-import { useState } from 'react';
-
-import { Contact } from '@/app/components/contact'
 
 
 export function Home() {
     const [name, setName] = useState("")
+
+    useEffect(() => {
+        fetchContacts()
+    },[])
 
     return (
         <View style={styles.container}>
